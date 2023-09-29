8dc331830afc86a899c5e744c55860d0b5a327ae
elndutra <elielton.dutra.morais@gmail.com>
2023-09-28 23:21:52 -0300

    Desafio Dio 1° projeto

---
+++ desafio-dio.playground/Contents.swift
@@ -0,0 +1,12 @@
+let name = "Steve"
+var lastName: String? = "Jobs"
+var defaultValue = "Wozniak"
+let unwrappedValue: String = lastName ?? defaultValue
+
+print("Este é o nome do criador da Apple \(name) \(unwrappedValue)")
+
+if let actualLastName = lastName {
+    print("Este é o nome do criador da Apple \(name) \(unwrappedValue)")
+} else {
+    print("Não contém uma String.")
+}
